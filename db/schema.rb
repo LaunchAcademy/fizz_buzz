fa038b773d53fc17b81cbd2a4f216cf9c30f95c3
Typhaon <davidgodwin788@gmail.com>
2014-06-24 13:10:50 -0400

    Installed Devise and created Devise views

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140623211318) do
+ActiveRecord::Schema.define(version: 20140624154633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,14 +60,23 @@ ActiveRecord::Schema.define(version: 20140623211318) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "first_name",                  null: false
-    t.string   "last_name",                   null: false
-    t.string   "email",                       null: false
-    t.string   "password",                    null: false
-    t.string   "role",       default: "user"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string   "first_name",                              null: false
+    t.string   "last_name",                               null: false
+    t.string   "role",                   default: "user", null: false
+    t.string   "email",                  default: "",     null: false
+    t.string   "encrypted_password",     default: "",     null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,      null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "votes", force: true do |t|
     t.integer  "score",      default: 0

d22414c201d1905af8488e57b35960cd0fa41aed
Nicholas Terrafranca <nterrafranca@gmail.com>
2014-06-26 09:00:48 -0400

    Added edit CRUD action.   Test passing!

--- spec/features/edit_review_spec.rb
+++ spec/features/edit_review_spec.rb
@@ -7,13 +7,21 @@ feature 'user edits review', %Q{As a user, I want to be able to edit my reviews.
   #   b. I can only edit reviews that I submitted
   #   c. After editing I can see the review in the list of reviews, the reviews are still in the same order
 
-  let(:review) { FactoryGirl.create(:review) }
+
+  let(:user) { FactoryGirl.create(:user) }
+  let(:brewery) { FactoryGirl.create(:brewery) }
+  # let(:review) { FactoryGirl.create(:review) } why is this not working?
 
   scenario 'User correctly edits review' do
-    login_as review.user
-    visit brewery_path(review.brewery)
-    click_on "Edit"
+    login_as user
+    review = FactoryGirl.build(:review)
+    review.brewery_id = brewery.id
+    review.user_id = user.id
+    review.save
+    visit brewery_path(brewery)
 
+    click_on "Edit"
+    save_and_open_page
     choose "review_rating_5"
     fill_in 'Body', with: 'This is a test string'
     click_on 'Update'
@@ -22,22 +30,25 @@ feature 'user edits review', %Q{As a user, I want to be able to edit my reviews.
     expect(page).to have_content '5'
     expect(page).to have_content "Review successfully updated"
 
-    save_and_open_page
   end
 
   scenario 'User incorrectly edits review' do
-    login_as review.user
-    visit brewery_path(review.brewery)
+    login_as user
+    review = FactoryGirl.build(:review)
+    review.brewery_id = brewery.id
+    review.user_id = user.id
+    review.save
+    visit brewery_path(brewery)
+
     click_on "Edit"
 
-    choose "review_rating_5"
     fill_in 'Body', with: ''
     click_on 'Update'
 
     expect(page).to have_content "can't be blank"
     expect(page).to_not have_content "Review successfully updated"
 
-    save_and_open_page
+
   end
 
 end

2def55162854081dd03afda4ef9f0e52d718474c
Nicholas Terrafranca <nterrafranca@gmail.com>
2014-06-26 11:56:05 -0400

    User can now only delete reviews that the user posted.

--- spec/features/delete_review_spec.rb
+++ spec/features/delete_review_spec.rb
@@ -13,17 +13,24 @@ feature 'User deletes a review', %Q{
 
   scenario 'User deletes a review' do
     login_as user
-    review = FactoryGirl.create(:review, user: user)
-    review.brewery_id = brewery.id
-    review.user_id = user.id
-    review.save
+    review = FactoryGirl.create(:review, user: user, brewery: brewery)
+
     visit brewery_path(brewery)
 
-    click_on 'Delete'
+    click_on 'Delete review'
 
     expect(page).to have_content "Review successfully deleted"
     expect(page).to_not have_content review.title
     expect(page).to_not have_content review.body
   end
 
+  scenario 'User can not delete a review that the user didnt create' do
+    user_2 = FactoryGirl.create(:user)
+    review = FactoryGirl.create(:review, user: user, brewery: brewery)
+
+    login_as user_2
+    visit brewery_path(brewery)
+
+    expect(page).to_not have_content 'Delete review'
+  end
 end

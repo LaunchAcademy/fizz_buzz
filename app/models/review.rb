c20e6abe455509b05fabf12b7603806983dd1a70
Nicholas Terrafranca <nterrafranca@gmail.com>
2014-06-27 10:50:48 -0400

    Added basic up/down voting functionality   Test are passing.   May need to provide more test conditions.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,32 @@
 class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :brewery
+  has_many :votes
+
   validates :title, presence: true
   validates :body, presence: true
   validates :rating, presence: true
+
+
+  def up_vote_count
+    total = 0
+    self.votes.each do |vote|
+      if vote.score == 1
+        total += 1
+      end
+    end
+    total
+
+
+  end
+
+  def down_vote_count
+    total = 0
+    self.votes.each do |vote|
+      if vote.score == -1
+        total -= 1
+      end
+    end
+    total
+  end
 end

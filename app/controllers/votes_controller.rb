b5f86dc056d4ced2cb4c7a5a71fac2c5598a1ec1
Nicholas Terrafranca <nterrafranca@gmail.com>
2014-06-27 14:04:08 -0400

    Cleaned up code per Hound's request.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,16 +1,16 @@
 class VotesController< ApplicationController
-
   def create
     @brewery = Brewery.find(params[:brewery_id])
     @review = Review.find(params[:review_id])
-    @vote = Vote.find_or_initialize_by(user_id: current_user.id, review_id: @review.id)
+    @vote = Vote.find_or_initialize_by(user_id: current_user.id,
+      review_id: @review.id)
 
     if @vote.update!(vote_params)
       redirect_to brewery_path(@brewery)
     elsif @vote.save
       redirect_to brewery_path(@brewery)
     else
-      render template: "breweries/show"
+      render template: 'breweries/show'
     end
   end
 

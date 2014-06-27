62c99054d37d6f0498c9da876df955c461f6c92f
Nicholas Terrafranca <nterrafranca@gmail.com>
2014-06-27 11:36:10 -0400

    Added a migration for an index linking user and review to a vote.   Cleaned up code.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140624154633) do
+ActiveRecord::Schema.define(version: 20140627152835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,5 +85,7 @@ ActiveRecord::Schema.define(version: 20140624154633) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "votes", ["user_id", "review_id"], name: "index_votes_on_user_id_and_review_id", unique: true, using: :btree
 
 end

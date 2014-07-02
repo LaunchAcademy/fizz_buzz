1bf9e04451dec28775a81426770c276eb7bd6c66
TeamRory <rorycteam@gmail.com>
2014-07-02 11:32:35 -0400

    Seeder is working.

--- db/schema.rb
+++ db/schema.rb
@@ -11,21 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140627152835) do
+ActiveRecord::Schema.define(version: 20140702151551) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "breweries", force: true do |t|
     t.string   "name",         null: false
-    t.text     "description",  null: false
+    t.text     "description"
     t.string   "address",      null: false
     t.string   "state",        null: false
     t.string   "city",         null: false
-    t.string   "zip",          null: false
+    t.string   "zip"
     t.string   "hours"
     t.integer  "user_id"
-    t.string   "phone_number", null: false
+    t.string   "phone_number"
     t.string   "url"
     t.datetime "created_at"
     t.datetime "updated_at"

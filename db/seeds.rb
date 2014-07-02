3ce7d9e0684e787bd52a3b01d7b48db4119dc4af
TeamRory <rorycteam@gmail.com>
2014-07-02 13:41:41 -0400

    God Damn hound

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
-require 'csv'
+require "csv"
 
-CSV.foreach('lib/assets/breweries_seeder.csv', headers: true) do |csv|
+CSV.foreach("lib/assets/breweries_seeder.csv", headers: true) do |csv|
   brewery = Brewery.find_or_initialize_by(name: csv["name"])
   brewery.city = csv["city"]
   brewery.address = csv["address1"]
